f38e42909b55fd609530c1a5490a2dbb6b3c3f76
VaclavSevcik <xsevci56@stud.fit.vutbr.cz>
2018-11-07 12:55:13 +0100

    Add change password without check current password and error messages.

--- app/src/main/java/cz/vutbr/fit/meetmeal/viewmodel/UserDetailViewModel.kt
+++ app/src/main/java/cz/vutbr/fit/meetmeal/viewmodel/UserDetailViewModel.kt
@@ -1,5 +1,7 @@
 package cz.vutbr.fit.meetmeal.viewmodel
 
+import android.app.Application
+import androidx.databinding.*
 import androidx.databinding.ObservableField
 import androidx.lifecycle.*
 import com.google.firebase.auth.*
@@ -7,12 +9,15 @@ import cz.vutbr.fit.meetmeal.engine.*
 import cz.vutbr.fit.meetmeal.model.User
 import io.reactivex.android.schedulers.*
 
-class UserDetailViewModel: ViewModel() {
+class UserDetailViewModel(application: Application): AndroidViewModel(application) {
 
   val user: ObservableField<User> = ObservableField(User())
   val firebaseUser: ObservableField<FirebaseUser> = ObservableField()
 
   private val userEngine = UserEngine()
+  val currentPass: ObservableField<String> = ObservableField<String>()
+  val newPass: ObservableField<String> = ObservableField<String>()
+  val newAgainPass: ObservableField<String> = ObservableField<String>()
 
   fun onScreenShowed() {
     firebaseUser.set(userEngine.getCurrentFirebaseUser())
@@ -29,7 +34,15 @@ class UserDetailViewModel: ViewModel() {
   }
 
   fun changePassword() {
-
+    // TODO zkontrolovani stavajiciho hesla
+    userEngine.checkCurrentPass(currentPass.get())
+    //userEngine.checkPassword() reautorizace
+    // TODO verifikovani noveho hesla - neni prazdne
+    if (newPass.get() != newAgainPass.get()){
+      // TODO chybova hlaska
+    } else {
+      userEngine.changePass(newPass.get().toString())
+    }
   }
 
 
